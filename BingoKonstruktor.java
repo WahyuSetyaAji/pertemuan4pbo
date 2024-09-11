3293e1204347009d9334980076593fbb0c420036
WahyuSetyaAji <wahyusetyaaji10@gmail.com>
2024-09-11 14:37:27 +0700

    first commit

---
+++ BingoKonstruktor.java
@@ -0,0 +1,12 @@
+package Pertemuan4;
+
+public class BingoKonstruktor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Bingo lagu = new Bingo();
+	      lagu.tampilkanLirik();
+	}
+
+}
+    
---
+++ InterfaceMatematika.java
@@ -0,0 +1,8 @@
+package Pertemuan4;
+
+public interface InterfaceMatematika {
+	public abstract int pertambahan();
+	public abstract int pengurangan();
+	public abstract int perkalian();
+	public abstract int pembagian();
+}
